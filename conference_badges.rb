31e32760390a8319f8457b5119c4a0e2d39cac80
Shujaat Azim <shujaat.azim@gmail.com>
2019-05-09 01:26:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,5 +7,5 @@ def batch_badge_creator(arr)
 end
 
 def assign_rooms(arr)
-  arr.each_with_index { |name, index| "Hello, #{name}! You'll be assigned to room #{index}!"}
+  arr.each_with_index { |name, index| "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
 end
