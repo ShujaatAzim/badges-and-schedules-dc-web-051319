7b2d4b9faf421ec11c51ecf411626bce73ee73d3
Shujaat Azim <shujaat.azim@gmail.com>
2019-05-09 01:16:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,7 @@
 def batch_badge_creator(arr)
   arr.map { |name| "Hello, my name is #{name}." }
 end
+
+def assign_rooms(arr)
+  arr.map { |name, index| "Hello, #{name}! You'll be assigned to room " + name[index] + "!"}
+end
